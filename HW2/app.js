92e219a25e2414789f3ac1e5200b0b65f0e1145c
OleksandraRiabchenko <79690371+OleksandraRiabchenko@users.noreply.github.com>
2021-08-20 18:41:07 +0300

    HW2_registration_login_allUsers globalFix

--- HW2/app.js
+++ HW2/app.js
@@ -21,24 +21,73 @@ app.get('/', (req, res) => {
     res.render('firstPage');
 })
 
-app.get('/registration', (req, res) => {
-    res.render('registration');
-})
+// app.get('/registration', (req, res) => {
+//     res.render('registration');
+// })
+//
+// app.post('/login', (req, res) => {
+//     const userExist = users.some(user => user.email === req.body.email);
+//
+//     if (userExist) {
+//         res.status(409).end('User with such mail exists');
+//         return;
+//     }
+//
+//     users.push(req.body);
+//     res.render('login');
+// });
+//
+// app.post('/users/:user_id', (req, res) => {
+//     const user = users.find(user => user.email === req.body.email && user.password === req.body.password);
+//
+//     if (!user) {
+//         res.status(404).end('User Not Found');
+//         return;
+//     }
+//
+//     res.render('userPage', {user});
+// });
+//
+// app.get('/users', (req, res) => {
+//     res.render('users', {users});
+// });
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
 
 app.post('/login', (req, res) => {
-    const userExist = users.some(user => user.email === req.body.email);
+    const {email, password} = req.body;
+    const user = users.find(user => user.email === email && user.password === password);
+
+    if (!user) {
+        res.status(409).end('Wrong email or password !');
+        return;
+    }
+
+    res.redirect('users');
+});
+
+app.post('/users', (req, res) => {
+    const {email} = req.body;
+    const userExist = users.some(user => user.email === email);
 
     if (userExist) {
-        res.status(409).end('User with such mail exists');
+        res.status(404).end('User exist, choose any email');
         return;
     }
 
     users.push(req.body);
-    res.render('login');
+    res.redirect('login');
 });
 
-app.post('/users/:user_id', (req, res) => {
-    const user = users.find(user => user.email === req.body.email && user.password === req.body.password);
+app.get('/users', (req, res) => {
+    res.render('users', {users});
+});
+
+app.get('/users/:user_id', (req, res) => {
+    const {user_id} = req.params;
+    const user = users.find((user, i) => i === +user_id);
 
     if (!user) {
         res.status(404).end('User Not Found');
@@ -48,9 +97,6 @@ app.post('/users/:user_id', (req, res) => {
     res.render('userPage', {user});
 });
 
-app.get('/users', (req, res) => {
-    res.render('users', {users});
-});
 
 app.listen(PORT, () => {
     console.log('App listen', PORT);
