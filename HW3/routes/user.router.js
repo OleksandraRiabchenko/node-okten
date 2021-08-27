433a2510820918546fbad9513843dbd008dc5ce1
OleksandraRiabchenko <79690371+OleksandraRiabchenko@users.noreply.github.com>
2021-08-27 11:20:16 +0300

    HW3 structure_linter not finished

--- HW3/routes/user.router.js
+++ HW3/routes/user.router.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { userController } = require('../controllers');
 
 router.get('/', userController.getAllUsers);
-router.post('/', userController.createUser);
+// router.post('/', userController.createUser);
 router.get('/:user_id', userController.getUserById);
 
 module.exports = router;
