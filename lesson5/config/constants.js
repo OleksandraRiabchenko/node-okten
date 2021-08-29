569ba1f7a73794283299c418ad6997157cc014e0
OleksandraRiabchenko <79690371+OleksandraRiabchenko@users.noreply.github.com>
2021-08-29 21:11:24 +0300

    lesson5 Joi_Bcrypt

---
+++ lesson5/config/constants.js
@@ -0,0 +1,4 @@
+module.exports = {
+    CURRENT_YEAR: new Date().getFullYear(),
+    PASSWORD_REGEXP: new RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\\$%\\^&\\*])(?=.{8,128})/),
+};
---
+++ lesson5/validators/user.validator.js
@@ -0,0 +1,28 @@
+const Joi = require('joi');
+const { CURRENT_YEAR } = require('../config/constants');
+const userRolesEnum = require('../config/user-roles.enum');
+
+const girlValidator = Joi.object({
+    name: Joi.string(),
+    age: Joi.number().min(15).max(60)
+});
+
+const createUserValidator = Joi.object({
+    name: Joi.string().alphanum().min(2).max(30)
+        .required(),
+    password: Joi.string().alphanum().min(8).max(128),
+    bor_year: Joi.number().min(CURRENT_YEAR - 120).max(CURRENT_YEAR - 6),
+    role: Joi.string().allow(...Object.values(userRolesEnum)),
+
+    car: Joi.boolean(),
+
+    girls: Joi.array()
+        .items(girlValidator)
+        .when('car', { is: true, then: Joi.required() })
+
+    // a: Joi.alternatives().conditional('b', { is: 5, then: Joi.string(), otherwise: Joi.number() })
+});
+
+module.exports = {
+    createUserValidator
+};
